b2494700834af1f9716e33e426c324b49a0302a3
Kevin Fong <kf53307@pausd.us>
2026-04-28 20:55:37 -0700

    initial commit

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,23 +1,14 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCsyeEScLUkaUukUmWYN4UFePwZMEK4STc",
-  authDomain: "rebuiltscouting.firebaseapp.com",
-  projectId: "rebuiltscouting",
-  storageBucket: "rebuiltscouting.firebasestorage.app",
-  messagingSenderId: "1047240648935",
-  appId: "1:1047240648935:web:5a91995b1852a759525a42",
-  measurementId: "G-8FC30CWN5L"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const alreadyInitialized = getApps().length > 0;
